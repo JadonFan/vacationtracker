4f3ebfac08c6833d7404d1582946b071efb87d7e
Jadon Fan <35983083+JadonFan@users.noreply.github.com>
2024-10-05 11:03:26 -0700

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# vacationtracker
+# Vacation Tracker
+
+In the [Discord developer portal](https://discord.com/developers/applications),
+
+1. Create a new application.
+2. Copy the application ID.
+3. Configure the privileged intents for the application.
+
+Then, in this project,
+
+1. Create a new file named .env in the same folder as this file.
+2. Paste the following line into the .env file and replace <application_id> with the application ID.
+
+    ```env
+    BOT_KEY=<application_id>
+    ```
+
+3. Run the following commands on the command line:
+
+    **Windows**
+
+    ```powershell
+    bot-env\Scripts\activate.bat
+    python3 -m vacationtracker
+    ```
+
+    **Mac**
+
+    ```bash
+    source bot-env/bin/activate
+    python3 -m vacationtracker
+    ```
---
+++ template.py
@@ -0,0 +1,76 @@
+from typing import Final
+
+from discord import Intents
+from discord.ext import commands
+
+intents = Intents.default()
+intents.members = True
+intents.message_content = True
+
+# The bot command has the format <command_prefix><command_name>. The current command
+# prefix is the question mark (?), but you can feel free to change it here to whatever
+# character you like.
+bot = commands.Bot(command_prefix='?', intents=intents)
+
+# Replace <command_name> with the name of your command.
+@bot.command(name='<command_name>')
+# Optional: Specify any additional parameters after the "ctx" in the following function signature.
+# Each function parameter corresponds to a parameter in your bot command.
+async def run_command(ctx):
+    # Get the message that triggered the execution of the bot command.
+    bot_msg = ctx.message
+    if not bot_msg:
+        raise Exception
+    
+    # Get the channel where the message was sent. From here, you can retrieve various
+    # channel info, such as all its messages, member info, etc.
+    channel = ctx.channel
+    if not channel:
+        raise Exception
+
+    # Get each message in the channel.
+    async for channel_msg in channel.history():
+        # Get the actual content of the message. USE THIS VARIABLE TO READ THE MESSAGE.
+        content = channel_msg.content
+
+        poll = channel_msg.poll
+        reaction = channel_msg.reaction
+        stickers = channel_msg.stickers
+
+        # Get the user who sent the channel message.
+        author = channel_msg.author
+        print(author)
+
+        avatar = author.avatar
+        bot = author.bot
+        display_name = author.display_name
+        global_name = author.global_name
+        name = author.name
+        joined_at = author.joined_at
+        status = author.status
+
+        # Write code here to do whatever you want to do with the message.
+        ...
+
+        # Replace <placeholder> with your favorite emoji.
+        emoji = "<placeholder>"
+        await channel_msg.add_reaction(emoji)
+
+        thread_name = "<placeholder>"
+        await channel_msg.create_thread(thread_name)
+
+        reply_content = None
+        await channel_msg.reply(reply_content)
+
+        await channel_msg.end_poll()
+
+        await channel_msg.pin()
+
+    # Replace <placeholder> with the message you want the bot to send back to
+    # the Discord channel.
+    out_msg = "<placeholder>"
+    await ctx.send(out_msg)
+
+
+BOT_ID: Final[int] = 0 # replace with your bot id
+bot.run(BOT_ID)
